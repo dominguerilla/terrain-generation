990f3ec90a3362d0d18f346891bf9b9a50d1b572
Carlos Dominguez <dominguerilla@gmail.com>
2019-03-12 19:50:08 -0400

    coloring the mesh

--- Assets/Mesh/Surface Shader.shader
+++ Assets/Mesh/Surface Shader.shader
@@ -20,6 +20,7 @@
 
 		struct Input {
 			float2 uv_MainTex;
+			half4 color: COLOR;
 		};
 
 		half _Glossiness;
@@ -36,11 +37,11 @@
 		void surf (Input IN, inout SurfaceOutputStandard o) {
 			// Albedo comes from a texture tinted by color
 			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
-			o.Albedo = c.rgb;
+			o.Albedo = c.rgb * IN.color.rgb;
 			// Metallic and smoothness come from slider variables
 			o.Metallic = _Metallic;
 			o.Smoothness = _Glossiness;
-			o.Alpha = c.a;
+			o.Alpha = c.a * IN.color.a;
 		}
 		ENDCG
 	}
--- Assets/Mesh/SurfaceCreator.cs
+++ Assets/Mesh/SurfaceCreator.cs
@@ -3,13 +3,36 @@
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class SurfaceCreator : MonoBehaviour {
 
+    public Vector3 offset;
+	public Vector3 rotation;
+
 	[Range(1, 200)]
 	public int resolution = 10;
+	public float frequency = 1f;
 	
+	[Range(1, 8)]
+	public int octaves = 1;
+	
+	[Range(1f, 4f)]
+	public float lacunarity = 2f;
+	
+	[Range(0f, 1f)]
+	public float persistence = 0.5f;
+	
+	[Range(1, 3)]
+	public int dimensions = 3;
+	
+	public NoiseMethodType type;
+	
+	public Gradient coloring;
+
 	private int currentResolution;
 
     private Mesh mesh;
-	
+	private Vector3[] vertices;
+	private Vector3[] normals;
+	private Color[] colors;
+    
 	private void OnEnable () {
 		if (mesh == null) {
 			mesh = new Mesh();
@@ -23,22 +46,43 @@ public class SurfaceCreator : MonoBehaviour {
 		if (resolution != currentResolution) {
 			CreateGrid();
 		}
+		Quaternion q = Quaternion.Euler(rotation);
+		Vector3 point00 = q * new Vector3(-0.5f, -0.5f) + offset;
+		Vector3 point10 = q * new Vector3( 0.5f, -0.5f) + offset;
+		Vector3 point01 = q * new Vector3(-0.5f, 0.5f) + offset;
+		Vector3 point11 = q * new Vector3( 0.5f, 0.5f) + offset;
+        
+		NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
+		float stepSize = 1f / resolution;
+		for (int v = 0, y = 0; y <= resolution; y++) {
+			Vector3 point0 = Vector3.Lerp(point00, point01, y * stepSize);
+			Vector3 point1 = Vector3.Lerp(point10, point11, y * stepSize);
+			for (int x = 0; x <= resolution; x++, v++) {
+				Vector3 point = Vector3.Lerp(point0, point1, x * stepSize);
+				float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+				sample = type == NoiseMethodType.Value ? (sample - 0.5f) : (sample * 0.5f);
+				vertices[v].y = sample;
+				colors[v] = coloring.Evaluate(sample + 0.5f);
+			}
+		}
+        mesh.vertices = vertices;
+		mesh.colors = colors;
 	}
 
 	public void CreateGrid() {
         currentResolution = resolution;
         mesh.Clear();
-        Vector3[] vertices = new Vector3[(resolution + 1) * (resolution + 1)];
-		Color[] colors = new Color[vertices.Length];
+        vertices = new Vector3[(resolution + 1) * (resolution + 1)];
+		colors = new Color[vertices.Length];
 		Vector2[] uv = new Vector2[vertices.Length];
-		Vector3[] normals = new Vector3[vertices.Length];
+		normals = new Vector3[vertices.Length];
         float stepSize = 1f / resolution;
-		for (int v = 0, y = 0; y <= resolution; y++) {
+		for (int v = 0, z = 0; z <= resolution; z++) {
 			for (int x = 0; x <= resolution; x++, v++) {
-				vertices[v] = new Vector3(x * stepSize - 0.5f, y * stepSize - 0.5f);
+				vertices[v] = new Vector3(x * stepSize - 0.5f, 0f, z * stepSize - 0.5f);
 				colors[v] = Color.black;
-				normals[v] = Vector3.back;
-				uv[v] = new Vector2(x * stepSize, y * stepSize);
+				normals[v] = Vector3.up;
+				uv[v] = new Vector2(x * stepSize, z * stepSize);
 			}
 		}
 		mesh.vertices = vertices;
