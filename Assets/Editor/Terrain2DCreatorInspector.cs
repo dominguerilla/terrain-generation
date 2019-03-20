851f75a7c94c31a6c0373e3f90846714d73b1656
cdom <dominguerilla@gmail.com>
2019-03-20 19:36:46 -0400

    init

---
+++ Assets/Editor/Terrain2DCreatorInspector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Terrain2DCreator))]
+public class Terrain2DCreatorInspector : Editor {
+
+    private Terrain2DCreator tc;
+
+    private void OnEnable() {
+        tc = (Terrain2DCreator)target;
+        Undo.undoRedoPerformed += RefreshCreator;
+    }
+
+    private void OnDisable() {
+        Undo.undoRedoPerformed -= RefreshCreator;
+    }
+
+    private void RefreshCreator(){
+        if(Application.isPlaying){
+            tc.FillTexture();
+        }
+    }
+
+    public override void OnInspectorGUI() {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if(EditorGUI.EndChangeCheck() && Application.isPlaying){
+            RefreshCreator();
+        }
+    }
+}
