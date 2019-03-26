35bc9ea8015ecd13cc101949d82c0e36fab8a394
Carlos Dominguez <dominguerilla@gmail.com>
2019-03-26 13:09:16 -0400

    prepping RefactoredTextureCreator for humidity gen

---
+++ Assets/Editor/RefactoredTextureCreatorInspector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RefactoredTextureCreator))]
+public class RefactoredTextureCreatorInspector : Editor {
+
+	private RefactoredTextureCreator creator;
+
+	private void OnEnable () {
+		creator = target as RefactoredTextureCreator;
+		Undo.undoRedoPerformed += RefreshCreator;
+	}
+
+	private void OnDisable () {
+		Undo.undoRedoPerformed -= RefreshCreator;
+	}
+
+	private void RefreshCreator () {
+		if (Application.isPlaying) {
+			creator.FillTexture();
+		}
+	}
+
+	public override void OnInspectorGUI () {
+		EditorGUI.BeginChangeCheck();
+		DrawDefaultInspector();
+		if (EditorGUI.EndChangeCheck()) {
+			RefreshCreator();
+		}
+	}
+}
--- Assets/RefactoredTextureCreator.cs
+++ Assets/RefactoredTextureCreator.cs
@@ -2,24 +2,12 @@
 
 public class RefactoredTextureCreator : MonoBehaviour {
 
+    public NoiseMaker noiseMaker;
+
 	[Range(2, 512)]
 	public int resolution = 256;
 
-	public float frequency = 1f;
-
-	[Range(1, 8)]
-	public int octaves = 1;
-
-	[Range(1f, 4f)]
-	public float lacunarity = 2f;
-
-	[Range(0f, 1f)]
-	public float persistence = 0.5f;
-
-	[Range(1, 3)]
-	public int dimensions = 3;
-
-	public NoiseMethodType type;
+    public Vector3 offset;
 
 	public Gradient coloring;
 
@@ -34,6 +22,7 @@ public class RefactoredTextureCreator : MonoBehaviour {
 			texture.anisoLevel = 9;
 			GetComponent<MeshRenderer>().material.mainTexture = texture;
 		}
+        noiseMaker.onChange.AddListener(FillTexture);
 		FillTexture();
 	}
 
@@ -49,20 +38,19 @@ public class RefactoredTextureCreator : MonoBehaviour {
 			texture.Resize(resolution, resolution);
 		}
 		
-		Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f,-0.5f));
-		Vector3 point10 = transform.TransformPoint(new Vector3( 0.5f,-0.5f));
-		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
-		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f));
+		Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f,-0.5f) + offset);
+		Vector3 point10 = transform.TransformPoint(new Vector3( 0.5f,-0.5f) + offset);
+		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f) + offset);
+		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f) + offset);
 
-		NoiseMethod method = Noise.methods[(int)type][dimensions - 1];
 		float stepSize = 1f / resolution;
 		for (int y = 0; y < resolution; y++) {
 			Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
 			Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
 			for (int x = 0; x < resolution; x++) {
 				Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
-				float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
-				if (type != NoiseMethodType.Value) {
+				float sample = noiseMaker.GetNoise(point);
+				if (noiseMaker.type != NoiseMethodType.Value) {
 					sample = sample * 0.5f + 0.5f;
 				}
 				texture.SetPixel(x, y, coloring.Evaluate(sample));
