15df5b0f1cf96c14eefaa863e241c680842f6b5c
cdom <dominguerilla@gmail.com>
2020-11-23 13:46:50 -0500

    Add flight-test
    Includes a 'plane' that has a constant thrust. Control yaw and pitch
    using vertical/horizontal axes.

---
+++ Assets/Flyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flyer : MonoBehaviour
+{
+    [SerializeField] float forwardSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.InverseTransformDirection(transform.forward) * Time.deltaTime * forwardSpeed);
+
+        //transform.Rotate(new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0));
+        transform.Rotate(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+    }
+}
