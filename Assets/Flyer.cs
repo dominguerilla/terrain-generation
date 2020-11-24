3685127f78b67d0cad80e9143d027450c210d144
cdom <dominguerilla@gmail.com>
2020-11-24 00:18:15 -0500

    Control roll with scroll wheel

--- Assets/Flyer.cs
+++ Assets/Flyer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Flyer : MonoBehaviour
 {
     [SerializeField] float forwardSpeed = 10f;
+    [SerializeField] float rollSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class Flyer : MonoBehaviour
         transform.Translate(transform.InverseTransformDirection(transform.forward) * Time.deltaTime * forwardSpeed);
 
         //transform.Rotate(new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0));
-        transform.Rotate(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+        transform.Rotate(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), Input.GetAxis("Mouse ScrollWheel") * rollSpeed );
     }
+
 }
